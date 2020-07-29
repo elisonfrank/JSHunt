49a1b82e08576bc7c41355a3aa05b8364b713d1b
elisonfrank <elisonfrank@hotmail.com>
2020-07-28 21:32:16 -0300

    Source

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Main from './pages/main';
+import Product from './pages/product';
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="JSHunt"
+          component={Main}
+          options={{
+            headerStyle: {backgroundColor: '#DA552F'},
+            headerTintColor: '#FFF',
+          }}
+        />
+
+        <Stack.Screen
+          name="Product"
+          component={Product}
+          options={({route}) => ({title: route.params.product.title})}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
